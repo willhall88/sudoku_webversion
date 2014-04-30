b5268c0c9554f06607b9051577787e9c08baee55
Robin <robindoble@gmail.com>
2014-04-30 17:10:50 +0100

    now sudoku highlights incorrect entries and distinguishes them from those supplied

--- sudoku.rb
+++ sudoku.rb
@@ -64,9 +64,9 @@ end
 
 helpers do 
   def colour_class(solution_to_check,puzzle_value,current_solution_value,solution_value)
-    must_be_guessed = puzzle_view == 0
-    tried_to_guess = current_solution_value.to_i != 0
-    guessed_incorrectly = current_solution_value != solution_value
+    must_be_guessed = (puzzle_value == "0")
+    tried_to_guess = (current_solution_value.to_i != 0)
+    guessed_incorrectly = (current_solution_value != solution_value)
 
     if solution_to_check &&
       must_be_guessed &&
@@ -76,8 +76,12 @@ helpers do
     elsif !must_be_guessed
       'value-provided'
     end
-    
   end
+
+  def cell_value(value)
+    value.to_i == 0 ? "" : value
+  end
+
 end
 
 
