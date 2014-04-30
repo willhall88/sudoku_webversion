350153959776c609c4c3dd3add06d88d030accaa
Robin <robindoble@gmail.com>
2014-04-30 17:54:50 +0100

    included gem partial

--- sudoku.rb
+++ sudoku.rb
@@ -63,7 +63,12 @@ get '/solution' do
 end
 
 helpers do 
-  def colour_class(solution_to_check,puzzle_value,current_solution_value,solution_value)
+ 
+  def cell_value(value)
+    value.to_i == 0 ? "" : value
+  end
+
+ def colour_class(solution_to_check,puzzle_value,current_solution_value,solution_value)
     must_be_guessed = (puzzle_value == "0")
     tried_to_guess = (current_solution_value.to_i != 0)
     guessed_incorrectly = (current_solution_value != solution_value)
@@ -76,10 +81,6 @@ helpers do
     elsif !must_be_guessed
       'value_provided'
     end
-  end
-
-  def cell_value(value)
-    value.to_i == 0 ? "" : value
   end
 
 end
